75d4ae4c398c84411f80a399348e6587c1ff881c
Gituser143 <bhargavsnv100@gmail.com>
2020-01-20 20:09:41 +0530

    Add Week2 Content

---
+++ Week2/W2P1-Variables.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This script displays various information to the screen.
+
+
+# Display 'Hello'
+echo 'Hello'
+
+# Assign a value to a variable.
+WORD='script'
+
+# Display that value using the variable.
+echo "$WORD"
+
+# Demonstrate that single quotes cause variables to not get expanded.
+echo '$WORD'
+
+# Combine the variable with hard-coded text.
+echo "This is a shell $WORD"
+
+# Display the contents of the variable using an alternative syntax.
+echo "This is a shell ${WORD}"
+
+# Append text to the variable.
+echo "${WORD}ing is fun!"
+
+# Show how NOT to append text to a variable.
+# This doesn't work:
+echo "$WORDing is fun!"
+
+# Create a new variable
+ENDING='ed'
+
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}."
+
+# Change the value stored in the ENDING variable.  (Reassignment.)
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!"
+
+# Reassign value to ENDING.
+ENDING='s'
+echo "You are going to write many ${WORD}${ENDING} in this class!"
+
+# Example of a special variable. The special variable here is ${UID}
+echo "The User ID of the current user is: ${UID}"
+
+# Examples of Command Execution
+
+# Using the $() syntax
+USER_NAME=$(id -un)
+echo 'Command executed is id -un'
+echo "Your username is ${USER_NAME}"
+
+#Using the back tick `` syntax
+USER_NAME=`whoami`
+echo 'Command used is whoami'
+echo "Your username is ${USER_NAME}"
